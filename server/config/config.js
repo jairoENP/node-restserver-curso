b3b0aed884ee605cf6d1d86068f817004fa0281a
bruvecha <bruvecha@gmail.com>
2020-06-10 00:50:15 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,27 @@
 //==========PORT=============
 process.env.PORT = process.env.PORT || 3000;
 //===========================
+
+
+//==========ENTORNO=============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+//==============================
+
+
+//==========BASE DE DATOS=============
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe' 
+}else{
+    urlDB = 'mongodb+srv://sinper:hPfPVOuDIf18lenC@cluster0-wkp0t.mongodb.net/cafe'
+}
+                       
+process.env.URLDB = urlDB;
+        
+
+//====================================
+ 
+
+//'mongodb://localhost:27017/cafe'
+//mongodb+srv://sinper:hPfPVOuDIf18lenC@cluster0-wkp0t.mongodb.net/cafe
