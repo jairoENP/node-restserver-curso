f69e4424fcb2df29bc3ccf00a34b4d3482dda37d
bruvecha <bruvecha@gmail.com>
2020-06-10 01:37:34 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,8 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe' 
 }else{
-    urlDB = 'mongodb+srv://sinper:hPfPVOuDIf18lenC@cluster0-wkp0t.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
+    // urlDB = 'mongodb+srv://sinper:hPfPVOuDIf18lenC@cluster0-wkp0t.mongodb.net/cafe'
 }
                        
 process.env.URLDB = urlDB;
